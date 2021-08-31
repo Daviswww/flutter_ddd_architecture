d3ebbfac0d0bdf89ef8709129efd671d5ed5d0c2
Hsing-Wei Ho <hwho@datalab.cs.nthu.edu.tw>
2021-08-31 23:39:19 +0800

    day26

--- lib/infrastructure/auth/auth_repository.dart
+++ lib/infrastructure/auth/auth_repository.dart
@@ -1,5 +1,4 @@
-import 'dart:developer';
-
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:dartz/dartz.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/presentation/screens/login/components/login_form.dart
+++ lib/presentation/screens/login/components/login_form.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stunning_tribble/application/authentication/authentication_bloc.dart';
 import 'package:stunning_tribble/application/sign_in/sign_in_bloc.dart';
-import 'package:stunning_tribble/presentation/router/router.gr.dart';
 import 'package:stunning_tribble/presentation/screens/login/components/google_sign_in_button.dart';
-import 'package:auto_route/auto_route.dart';
 
 class LoginFrom extends StatelessWidget {
   const LoginFrom({Key? key}) : super(key: key);
